18e706f5866847b5e8511ee4faa94b5d7df9d8b2
Edson Lima <edsonlima@edson-notebook.(none)>
2014-03-01 10:21:33 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adnconsult::Application.config.secret_token = '1fe6e8863fbbf6dec6aadaa399a5e978d67048e3f6aef1b4ddf80090747b1ec9c2ad873d11f1b1258ad53aa28de309b5c28ada4c711c34a3321c7bd61b81cb34'
